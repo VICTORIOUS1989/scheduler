88b55c58285259f718244f660a18d5be44724aa5
FAYZA DAOUDI <foufadaoudi@gmail.com"git config --global user.email foufadaoudi@gmail.comgit config --global user.name 'Victorious1989'git config --global user.email 'foufadaoudi@gmail.com>
2020-12-10 15:39:04 -0500

    Managing State

--- src/components/Application.js
+++ src/components/Application.js
@@ -49,8 +49,17 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("");
-  const [days, setDays] = useState([]);
+  const setDay = day => setState({ ...state, day });
+
+  const setDays = (days) => { setState(prev => ({ ...prev, days }));};
+
+  const [state, setState] = useState({
+    day: "",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+   // appointments: {}
+  });
+  
       useEffect(() => {
       const testURL = `http://localhost:8001/api/days`;
       axios.get(testURL).then(response => {
@@ -76,8 +85,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
             <DayList
-                days={days}
-                day={day}
+                days={state.days}
+                day={state.day}
                 setDays={setDays}
                 setDay={setDay}
               />
