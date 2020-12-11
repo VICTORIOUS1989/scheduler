94422970ebed4c4887c5ce3cfabba456c0fe41a7
FAYZA DAOUDI <foufadaoudi@gmail.com"git config --global user.email foufadaoudi@gmail.comgit config --global user.name 'Victorious1989'git config --global user.email 'foufadaoudi@gmail.com>
2020-12-11 12:27:20 -0500

    Building a Hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,28 @@
+import React, { useState} from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]); // This line is new!
+
+  const transition = function(newMode,replace=false ) {
+    if (!replace){
+      setMode(newMode);
+      history.push(newMode);
+    } else {
+      setMode(newMode);
+    }
+  
+  }
+
+  const back = function() {
+    if (history.length > 1){
+      history.pop();
+      setMode(history[history.length - 1]);
+    }
+  
+  }
+
+  return { mode ,transition, back };
+}
+
+
